0dec73af7f976853bd3bf8e6ec7b5685c9e093e2
ndk1195 <ndkhoa1195@gmail.com>
2025-01-10 15:09:59 +0700

    finish header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./index.html"],
   theme: {
     extend: {
       colors: {
-        black: "#151515",
+        "black-15": "#151515",
         green: "#4EE1A0",
         "dark-grey": "#242424",
         grey: "#D9D9D9",
+      },
+      backgroundImage: {
+        "profile-mobile": "url('/images/image-profile-mobile.webp')",
+        "profile-tablet": "url('/images/image-profile-tablet.webp')",
+        "profile-desktop": "url('/images/image-profile-desktop.webp')",
       },
     },
   },
